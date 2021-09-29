557224444b3ec9e03b546df1ec1aa8cd1e23a60b
Sreekuttansree <88297456+Sreekuttansree@users.noreply.github.com>
2021-09-29 21:41:52 +0530

    https://github.com/Sreekuttansree/vijayrockz
    yes

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '🔰Main Channel🔰', url="https://t.me/MovieRockz2"
                                 )
                         ]
                     ]
